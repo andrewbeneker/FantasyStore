e5b878f5feec993e28a9f06397f81366f89ac7a4
Andrew Beneker <andrewbeneker@gmail.com>
2025-02-26 21:57:43 -0500

    scaffolding to API

---
+++ FantasyStoreAPI/Entities/Inventory.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace FantasyStoreAPI.Entities;
+
+public partial class Inventory
+{
+    public int Id { get; set; }
+
+    public string ItemName { get; set; } = null!;
+
+    public int StockQuantity { get; set; }
+
+    public int RestockThreshold { get; set; }
+
+    public virtual ICollection<Order> Orders { get; set; } = new List<Order>();
+}
---
+++ FantasyStoreAPI/Entities/Order.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace FantasyStoreAPI.Entities;
+
+public partial class Order
+{
+    public int Id { get; set; }
+
+    public int? ItemId { get; set; }
+
+    public int Quantity { get; set; }
+
+    public DateTime? OrderDate { get; set; }
+
+    public virtual Inventory? Item { get; set; }
+}
